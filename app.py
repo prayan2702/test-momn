3773a069a7563f0e5c416025cfe2be1d602326bb
prayan2702 <165507774+prayan2702@users.noreply.github.com>
2025-01-07 16:38:29 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,6 +3,9 @@ from momn_streamlit_app import main as momn_main
 from Strategy_performance import main as strategy_main
 from strategy_tearsheet import main as tearsheet_main
 
+# Set page configuration to wide layout (this must be the first Streamlit command)
+st.set_page_config(page_title="Portfolio Report", layout="wide")
+
 # Sidebar Navigation
 st.sidebar.title("Navigation")
 pages = {
