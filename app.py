2f3c13adf300995b82eb3ad885cee287c1335e26
prayan2702 <165507774+prayan2702@users.noreply.github.com>
2025-01-07 23:20:39 +0530

    Update app.py

--- app.py
+++ app.py
@@ -6,16 +6,21 @@ from strategy_tearsheet import main as tearsheet_main
 # यह पहली Streamlit कमांड होनी चाहिए
 st.set_page_config(page_title="Portfolio Report", layout="wide")
 
-# Custom CSS to adjust layout
 hide_streamlit_style = """
     <style>
     div[data-testid="stSidebarNav"] {display: none !important;}
     div[data-testid="stHeader"] {display: none !important;}
-    div.block-container {padding-top: 0rem !important; padding-bottom: 0rem !important;}
-    section.main > div:has(~ footer ) {padding-top: 0rem !important;}
+    div.block-container {
+        padding-top: 2rem !important; /* Adjust top padding */
+        padding-bottom: 0rem !important;
+    }
+    section.main > div:has(~ footer ) {
+        padding-top: 2rem !important; /* Adjust main section's top padding */
+    }
     </style>
     """
 st.markdown(hide_streamlit_style, unsafe_allow_html=True)
+
 
 # Navigation Tabs
 tab1, tab2, tab3 = st.tabs(["Momentum App", "Strategy Performance", "Strategy Tearsheet"])
