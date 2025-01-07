e66c832a3dc3706d9ae81ffe9cecefc58c3a3391
prayan2702 <165507774+prayan2702@users.noreply.github.com>
2025-01-07 23:23:29 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,24 +3,24 @@ from momn_streamlit_app import main as momn_main
 from Strategy_performance import main as strategy_main
 from strategy_tearsheet import main as tearsheet_main
 
-# # यह पहली Streamlit कमांड होनी चाहिए
-# st.set_page_config(page_title="Portfolio Report", layout="wide")
+# Set the page layout to wide
+st.set_page_config(page_title="Portfolio Report", layout="wide")
 
+# Adjust CSS to fix top cutoff issue
 hide_streamlit_style = """
     <style>
     div[data-testid="stSidebarNav"] {display: none !important;}
     div[data-testid="stHeader"] {display: none !important;}
     div.block-container {
-        padding-top: 2rem !important; /* Adjust top padding */
-        padding-bottom: 0rem !important;
+        padding-top: 4rem !important; /* Increase top padding to avoid cutoff */
+        padding-bottom: 1rem !important;
     }
     section.main > div:has(~ footer ) {
-        padding-top: 2rem !important; /* Adjust main section's top padding */
+        padding-top: 4rem !important; /* Adjust main section's top padding */
     }
     </style>
     """
 st.markdown(hide_streamlit_style, unsafe_allow_html=True)
-
 
 # Navigation Tabs
 tab1, tab2, tab3 = st.tabs(["Momentum App", "Strategy Performance", "Strategy Tearsheet"])
