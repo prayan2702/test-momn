92f1b3065c800e27767391701a8ca228f4f01eda
prayan2702 <165507774+prayan2702@users.noreply.github.com>
2025-01-07 17:35:06 +0530

    Update app.py

--- app.py
+++ app.py
@@ -6,7 +6,8 @@ from strategy_tearsheet import main as tearsheet_main
 # # Set page configuration to wide layout (this must be the first Streamlit command)
 # st.set_page_config(page_title="Portfolio Report", layout="wide")
 
-# संशोधित CSS कोड
+
+# CSS कोड
 hide_streamlit_style = """
             <style>
             div[data-testid="stSidebarNav"] {display: none;}
@@ -15,15 +16,25 @@ hide_streamlit_style = """
             """
 st.markdown(hide_streamlit_style, unsafe_allow_html=True)
 
+# सत्र स्थिति में वर्तमान पृष्ठ
+if "current_page" not in st.session_state:
+    st.session_state.current_page = "Momentum App"
+
 # नेविगेशन टैब
 tab1, tab2, tab3 = st.tabs(["Momentum App", "Strategy Performance", "Strategy Tearsheet"])
 
-# प्रत्येक टैब के लिए सामग्री
-with tab1:
+# टैब 1 के लिए
+if tab1:
+    st.session_state.current_page = "Momentum App"
+if tab2:
+    st.session_state.current_page = "Strategy Performance"
+if tab3:
+    st.session_state.current_page = "Strategy Tearsheet"
+
+# चयनित पृष्ठ लोड करें
+if st.session_state.current_page == "Momentum App":
     momn_main()
-
-with tab2:
+elif st.session_state.current_page == "Strategy Performance":
     strategy_main()
-
-with tab3:
+elif st.session_state.current_page == "Strategy Tearsheet":
     tearsheet_main()
