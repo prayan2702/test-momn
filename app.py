e45516b97570df48176530753b9f3132f8bed66d
prayan2702 <165507774+prayan2702@users.noreply.github.com>
2025-01-07 17:21:54 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,8 +3,8 @@ from momn_streamlit_app import main as momn_main
 from Strategy_performance import main as strategy_main
 from strategy_tearsheet import main as tearsheet_main
 
-# # Set page configuration to wide layout (this must be the first Streamlit command)
-# st.set_page_config(page_title="Portfolio Report", layout="wide")
+# Set page configuration to wide layout (this must be the first Streamlit command)
+st.set_page_config(page_title="Portfolio Report", layout="wide")
 
 # नेविगेशन टैब
 tab1, tab2, tab3 = st.tabs(["Momentum App", "Strategy Performance", "Strategy Tearsheet"])
