405fa4e69c5ee01beec0dca9476dd2679f9b74e4
prayan2702 <165507774+prayan2702@users.noreply.github.com>
2025-01-07 23:22:16 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,8 +3,8 @@ from momn_streamlit_app import main as momn_main
 from Strategy_performance import main as strategy_main
 from strategy_tearsheet import main as tearsheet_main
 
-# यह पहली Streamlit कमांड होनी चाहिए
-st.set_page_config(page_title="Portfolio Report", layout="wide")
+# # यह पहली Streamlit कमांड होनी चाहिए
+# st.set_page_config(page_title="Portfolio Report", layout="wide")
 
 hide_streamlit_style = """
     <style>
